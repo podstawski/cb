deab282219a23c8642f5b9ee68989465c6dc3791
Piotr Podstawski <piotr@webkameleon.com>
2016-04-11 16:09:35 +0000

    login

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var crypto = require('crypto');
+
 app.use(express.static('public'));
 
 
@@ -8,19 +10,66 @@ var io = require('socket.io').listen(app.listen(3000, function () {
   console.log('App listening on port 3000!');
 }));
 
-var sockets=[];
+
+var session=[];
+
+function parseCookies (rc) {
+    var list = {};
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+}
 
 io.sockets.on('connection', function (socket) {
-  console.log('Hello new client');
+  var hash='#';
+  var cookies=parseCookies(socket.handshake.headers.cookie);
+    
+  console.log('Hello new client',session);
   
-  setTimeout(function() {
-    socket.emit('message', { message: 'welcome my old friend' });
-  },2000);
+  if (typeof(cookies.sessid)!='undefined')
+    if (typeof(session[cookies.sessid])!='undefined')
+      hash=cookies.sessid;
+      
+  if (hash.length>1) {
+    socket.emit('login',session[hash]);
+  } else {
+    socket.emit('logout');
+  }
+  
   
   socket.on('disconnect',function() {
     console.log('Bye client');
+  });
+  
+  socket.on('login',function (data) {
+    
+    var md5sum = crypto.createHash('md5');
+    md5sum.update(data.username + new Date());
+    hash=md5sum.digest('hex');
+    
+    if (data.username==data.password) {
+      session[hash]=data;
+      socket.emit('cookie','sessid',hash);
+      socket.emit('login',data);
+            
+    } else {
+      socket.emit('err','Login error',"Username or password doesn't match");
+    }
     
   });
+  
+  socket.on('logout',function() {
+    
+    if (typeof(session[hash])) {
+      delete(session[hash]);
+      socket.emit('logout');
+    }
+  });
+  
 });
 
 
--- public/assets/js/controlby.js
+++ public/assets/js/controlby.js
@@ -1,8 +1,49 @@
-var io_socket = io.connect();
 
-io_socket.on ('message',function(msg) {
-    toastr.success(msg.message, 'Simon sais', {
+
+
+var websocket = io.connect();
+
+websocket.on('err',function(err,details) {
+    toastr.error(details, err, {
         closeButton: true,
         progressBar: true,
     });
+});
+
+
+websocket.on('cookie',function(cn,cv) {
+    document.cookie=cn+'='+cv;
+});
+
+websocket.on('login',function(data) {
+    console.log(data);
+    $('#username').text(data.username);
+    $('.after-login').removeClass('after-login').addClass('_after-login');
+    $('body').addClass('sidebar-nav').removeClass('sidebar-off-canvas');
+    loadPage('dashboard.html');
+});
+
+$('#logout').click(function () {
+    websocket.emit('logout');
+});
+
+websocket.on('logout',function() {
+    $('#username').text('nobody');
+    $('._after-login').removeClass('_after-login').addClass('after-login');
+    $('body').removeClass('sidebar-nav').addClass('sidebar-off-canvas');
+    loadPage('main.html');
+});
+
+$(function(){
+    
+    (function() {
+        var loadPageParent = window.loadPage;
+        
+        window.loadPage = function(url) {
+            console.log(url);
+            loadPageParent(url);        
+        }
+    
+    })();
+
 });
