2d0d23586883af213b30a2b34e2bb6668a93c576
Piotr Podstawski <piotr@webkameleon.com>
2016-04-15 09:00:40 +0000

    walka z svg

--- public/assets/js/views/floor.js
+++ public/assets/js/views/floor.js
@@ -1,5 +1,87 @@
+var polygonMode=true;
+var polygonPoints=[];
+var lastDragEvent=0;
+var dotW=16;
+var dotH=16;
+
+
+var zoomContainer = function(z) {
+    
+    var sel='#floor-container .draggable-container';
+    var current=$(sel).css('zoom');
+    
+    
+
+    if (current===undefined) {
+        current=$(sel).css('-moz-transform');
+        if (current===undefined || current=='none') current=1;
+        else {
+            current=current.substr(7);
+            current=current.split(',');
+            current=parseFloat(current[0]);
+        }
+    } else if (current.indexOf('%')>0) {
+        current=current.replace('%','');
+        current=parseFloat(current)/100;
+    } else {
+        current=parseFloat(current);
+    }
+ 
+    
+    if (z!=null) {
+        current*=z;
+        $(sel).css('-moz-transform','scale('+current+')');
+        $(sel).css('zoom',current);
+        
+    }    
+    
+    return current;    
+
+}
+
+var calulatePoint = function(p) {
+    var zoom=zoomContainer();
+    var w=$('#floor-container .draggable-container').width();
+    var h=$('#floor-container .draggable-container').height();
+
+    var point={
+        x: p.x*w*zoom,
+        y: p.y*h
+    };
+    
+    return point;
+}
+
+var drawPolygonPoints = function() {
+
+    
+    var xs=[];
+    for (var i=0; i<polygonPoints.length; i++) {
+    
+        xs.push(polygonPoints[i].x);
+        var p=calulatePoint(polygonPoints[i]);
+        var x=p.x - (dotW/2);
+        var y=p.y - (dotH/2);
+        
+        polygonPoints[i].dot.css({left: x, top: y});
+        
+    }
+    
+    //console.log(xs);
+    //console.log(w,h,w*zoom,h*zoom);
+}
+
+var createPolygonFromPoints = function() {
+    
+    for (var i=0; i<polygonPoints.length; i++) {
+
+        polygonPoints[i].dot.remove();
+    }
+    polygonPoints=[];   
+}
+
 var floorDraw=function(data) {
-    console.log(data);
+    
     
     $('.page-header .page-title').text(data.name);
     $('.page-header .page-desc').text(data.desc);
@@ -10,6 +92,32 @@ var floorDraw=function(data) {
     }
     
     $('#floor-container img.svg').attr('src',data.img);
+    
+    $('#floor-container .svg').click(function(e){
+        if (polygonMode && Date.now()-lastDragEvent>800) {
+            var zoom=zoomContainer();
+            var ex = parseFloat(e.offsetX === undefined ? e.originalEvent.layerX : e.offsetX);
+            var ey = parseFloat(e.offsetY === undefined ? e.originalEvent.layerY : e.offsetY);
+            var w=parseFloat($('#floor-container').width());
+            var h=parseFloat($('#floor-container .draggable-container').height());
+            var point={
+                x:(ex/zoom)/w,
+                y:(ey/zoom)/h
+            }
+            
+            var img='<img src="assets/img/dot.png" class="polygon-dot"/>';
+            
+            
+            point.dot=$(img).css({left: -1*dotW, top: -1*dotH}).appendTo('#floor-container .draggable-container');
+            
+            polygonPoints.push(point);
+            point.dot.attr('title',polygonPoints.length);
+            point.dot.click(createPolygonFromPoints);
+            drawPolygonPoints();
+        }
+    
+    });
+    
     
 }
 
@@ -40,23 +148,30 @@ $(function(){
     
     
     
-    $('#floor-container .draggable-container').draggable();
+    $('#floor-container .draggable-container').draggable({
+        stop: function() {
+            lastDragEvent=Date.now();
+        }
+    });
     
     
     $('#floor-container .draggable-container').bind('mousewheel', function(e){
-        console.log(this);
-        if(e.originalEvent.wheelDelta /120 > 0) {
-            $(this).css('zoom',parseFloat($(this).css('zoom'))*1.1);
-        }
-        else{
-            $(this).css('zoom',parseFloat($(this).css('zoom'))*0.9);
-        }
+        
+        if(e.originalEvent.wheelDelta /120 > 0) zoomContainer(1.1);
+        else zoomContainer(0.9);
+        
+    }).bind('DOMMouseScroll',function(e) {
+        if (e.detail<0) zoomContainer(1.1);
+        if (e.detail>0) zoomContainer(0.9);
+        
     });   
     
     
     var calculateWH = function () {
         $('#floor-container').height(parseInt($(window).height())-270);
-        $('.svg').width($('#floor-container .draggable-container').width());
+        $('.svg').width($('#floor-container').width());
+        
+        drawPolygonPoints();
     }
 
     calculateWH();
